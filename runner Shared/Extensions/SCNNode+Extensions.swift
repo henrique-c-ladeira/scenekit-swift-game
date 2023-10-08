f6108d2e33ac29d4d1aeb7ca981523c761312355
Henrique <henrique.c.ladeira@gmail.com>
2023-10-08 17:05:31 -0300

    init

---
+++ runner Shared/Extensions/SCNNode+Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  SCNNode+Extensions.swift
+//  runner
+//
+//  Created by Henrique Ladeira on 08/10/23.
+//
+
+import SceneKit
+
+extension SCNNode {
+    func anchor(to node: SCNNode) {
+        self.position = SCNVector3(x: node.position.x, y: node.position.y + 1, z: node.position.z - 3)
+        self.rotation = SCNVector4(x: 0, y: Float.pi, z: 0, w: Float.pi)
+        let pointOfView = SCNNode()
+        pointOfView.position = node.position
+        pointOfView.position.z = node.position.z + 20
+        
+        constraints = [SCNLookAtConstraint(target: pointOfView)]
+        
+    }
+}
--- runner Shared/GameController.swift
+++ runner Shared/GameController.swift
@@ -14,21 +14,42 @@ import SceneKit
 #endif
 
 class GameController: NSObject, SCNSceneRendererDelegate {
-
+    
     let scene: SCNScene
     let sceneRenderer: SCNSceneRenderer
     
+    let player: SCNNode
+    let camera: SCNNode
+    
     init(sceneRenderer renderer: SCNSceneRenderer) {
         sceneRenderer = renderer
-        scene = SCNScene(named: "Art.scnassets/ship.scn")!
+        scene = SCNScene(named: "Art.scnassets/stage1.scn")!
+        
+        self.player = scene.rootNode.childNode(withName: "player", recursively: true)!
+        self.camera = scene.rootNode.childNode(withName: "camera", recursively: true)!
         
         super.init()
         
         sceneRenderer.delegate = self
         
-        if let ship = scene.rootNode.childNode(withName: "ship", recursively: true) {
-            ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+        camera.anchor(to: player)
+        
+        let floor = scene.rootNode.childNode(withName: "plane", recursively: true)!
+        floor.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(geometry: floor.geometry!))
+        
+        
+        for index in (0...10) {
+            let block = scene.rootNode.childNode(withName: "blocker", recursively: true)!
+            let newBlock = SCNNode(geometry: block.geometry);
+            newBlock.position = block.position
+            newBlock.position.z = block.position.z + 5 * Float(index)
+            newBlock.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(geometry: newBlock.geometry!))
+            scene.rootNode.addChildNode(newBlock)
         }
+        
+        player.runAction(SCNAction.repeatForever(SCNAction.moveBy(x: 0.01, y: 0, z: 3, duration: 1)))
+        player.physicsBody = SCNPhysicsBody(type: .kinematic, shape: SCNPhysicsShape(geometry: player.geometry!))
+        
         
         sceneRenderer.scene = scene
     }
@@ -63,6 +84,7 @@ class GameController: NSObject, SCNSceneRendererDelegate {
     
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
         // Called before each frame is rendered
+        camera.anchor(to: player)
     }
 
 }
